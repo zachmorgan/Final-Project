aaa8f8d369a9a5dc31f55c7bf255e2fe94cad573
Kaori Price <kaori.price@gmail.com>
2013-05-29 20:58:15 -0700

    Initial commit

---
+++ cyoa/slides/js/president.js
@@ -0,0 +1,23 @@
+﻿
+Slides.president = new Slide();
+
+Slides.president.Load = function () {
+    $("button").click(function () {
+        GoToSlide("president");
+    });
+
+    $("p").hide();
+};
+
+Slides.president.Unload = function () {
+    $("button").unbind("click");
+};
+
+Slides.president.Show = function () {
+    return $("figure").fadeIn().promise();
+};
+
+Slides.president.Hide = function () {
+    return $("figure").fadeOut().promise();
+};
+
