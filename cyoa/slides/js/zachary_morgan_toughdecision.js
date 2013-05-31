49d069c6357527692b1d1c16a97d5f8137dd74b4
Zachary Morgan <zmorgandesign@hotmail.com>
2013-05-30 19:17:40 -0700

    zach's base files
    adding files for my slides

---
+++ cyoa/slides/js/zachary_morgan_toughdecision.js
@@ -0,0 +1,23 @@
+﻿
+Slides.start = new Slide();
+
+Slides.start.Load = function () {
+    $("button").click(function() {
+        GoToSlide("president");
+    });
+
+    $("p").hide();
+};
+
+Slides.start.Unload = function() {
+    $("button").unbind("click");
+};
+
+Slides.start.Show = function () {
+    return $("figure").fadeIn().promise();
+};
+
+Slides.start.Hide = function () {
+    return $("figure").fadeOut().promise();
+};
+
