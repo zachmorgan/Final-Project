d5b333a0ce5f46d421eafc7c5efbcfdb04abd4a0
Kay W <kaylaenojado@gmail.com>
2013-06-02 13:50:23 -0700

    these are my files

---
+++ cyoa/slides/js/kay_enojado_still_too_late.js
@@ -0,0 +1,23 @@
+﻿
+Slides.start = new Slide();
+
+Slides.start.Load = function () {
+    $("button").click(function() {
+        GoToSlide("president");
+    });
+
+    $("p").hide();
+};
+
+Slides.start.Unload = function() {
+    $("button").unbind("click");
+};
+
+Slides.start.Show = function () {
+    return $("p").fadeIn().promise();
+};
+
+Slides.start.Hide = function () {
+    return $("p").fadeOut().promise();
+};
+
