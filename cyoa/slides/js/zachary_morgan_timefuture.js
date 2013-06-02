63b3a274245834e7bc739f718b83e980a15a4362
Zachary Morgan <zmorgandesign@hotmail.com>
2013-06-02 16:20:23 -0700

    Master file update

--- cyoa/slides/js/zachary_morgan_timefuture.js
+++ cyoa/slides/js/zachary_morgan_timefuture.js
@@ -1,17 +1,4 @@
-﻿if (!String.prototype.supplant) {
-    String.prototype.supplant = function (o) {
-        return this.replace(
-            /\{([^{}]*)\}/g,
-            function (a, b) {
-                var r = o[b];
-                return typeof r === 'string' || typeof r === 'number' ? r : a;
-            }
-        );
-    };
-}
-
-
-function LoadSvg(svg, url) {
+﻿function LoadSvg(svg, url) {
     var d = $.Deferred();
 
     svg.load(url, function () {
