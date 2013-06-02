ea92403cd7311dd55e2929900f985b56043c46dc
Zachary Morgan <zmorgandesign@hotmail.com>
2013-06-02 15:56:04 -0700

    SVG file group name updates

--- cyoa/slides/js/zachary_morgan_timefuture.js
+++ cyoa/slides/js/zachary_morgan_timefuture.js
@@ -1,7 +1,52 @@
-﻿
+﻿if (!String.prototype.supplant) {
+    String.prototype.supplant = function (o) {
+        return this.replace(
+            /\{([^{}]*)\}/g,
+            function (a, b) {
+                var r = o[b];
+                return typeof r === 'string' || typeof r === 'number' ? r : a;
+            }
+        );
+    };
+}
+
+
+function LoadSvg(svg, url) {
+    var d = $.Deferred();
+
+    svg.load(url, function () {
+        d.resolve();
+    });
+
+    return d.promise();
+}
+
 Slides.zachary_morgan_timefuture = new Slide();
 
 Slides.zachary_morgan_timefuture.Load = function () {
+	var d = $.Deferred();
+
+    $("#svgcontainer").svg(function (svg) {
+
+        Slides.zachary_morgan_toughdecision.svg = svg;
+
+        var svgsToLoad = [];
+
+        svgsToLoad.push(LoadSvg(svg, "/slides/resources/morgan_earth.svg"));
+		svgsToLoad.push(LoadSvg(svg, "/slides/resources/morgan_teleflash.svg"));
+		svgsToLoad.push(LoadSvg(svg, "/slides/resources/morgan_damagedearth1.svg"));
+		svgsToLoad.push(LoadSvg(svg, "/slides/resources/morgan_damagedearth2.svg"));
+		svgsToLoad.push(LoadSvg(svg, "/slides/resources/morgan_damagedearth3.svg"));
+		svgsToLoad.push(LoadSvg(svg, "/slides/resources/morgan_damagedearth4.svg"));
+		svgsToLoad.push(LoadSvg(svg, "/slides/resources/morgan_timetraveler.svg"));
+		
+
+        $.when.apply($, svgsToLoad).done(function () {
+            d.resolve();
+        });
+    });
+
+    return d.promise();
     $("p").hide();
 };
 
@@ -10,6 +55,11 @@ Slides.zachary_morgan_timefuture.Unload = function() {
 };
 
 Slides.zachary_morgan_timefuture.Show = function () {
+	var wearth = ("g#earth");
+	
+	wearth.attr("opacity", "1");
+    wearth.attr("transform", "translate(310,230)");
+	
     return $("figure").fadeIn().promise();
 };
 
