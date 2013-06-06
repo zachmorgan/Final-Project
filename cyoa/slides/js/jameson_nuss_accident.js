1ae6be004d695b21798fdd7cb7c798ddcf218d49
Jameson Nuss <jamesonnuss@yahoo.com>
2013-06-05 20:22:18 -0700

    Fixed Zach's Problems    :)

--- cyoa/slides/js/jameson_nuss_accident.js
+++ cyoa/slides/js/jameson_nuss_accident.js
@@ -1,25 +1,53 @@
 // jameson_nuss_accident
 
 
-Slides.start = new Slide();
+Slides.jameson_nuss_accident = new Slide();
 
-Slides.start.Load = function () {
-    $("button").click(function() {
-        GoToSlide("president");
+Slides.jameson_nuss_accident.Load = function () {
+
+    var d = $.Deferred();
+
+    //Make sure to add <div id="svgcontainer"></div> 
+    $("#svgcontainer").svg(function (svg) {
+
+        var svgsToLoad = [];
+
+        svgsToLoad.push(LoadSvg(svg, "/slides/resources/jameson_nuss_barrel.svg"));
+        svgsToLoad.push(LoadSvg(svg, "/slides/resources/jameson_nuss_caution_tape.svg"));
+        svgsToLoad.push(LoadSvg(svg, "/slides/resources/jameson_nuss_outline.svg"));
+
+        $.when.apply($, svgsToLoad).done(function () {
+            d.resolve();
+        });
     });
 
-    $("p").hide();
+    return d.promise();
 };
 
-Slides.start.Unload = function() {
-    $("button").unbind("click");
+Slides.jameson_nuss_accident.Unload = function () {
 };
 
-Slides.start.Show = function () {
-    return $("p").fadeIn().promise();
+Slides.jameson_nuss_accident.Show = function () {
+    var d = $.Deferred();
+
+    var barrel = $("g#Barrel");
+    var glob = $("g#Greenglob");
+
+    barrel.attr("transform", "translate(500,230) scale(5,5)");
+
+    function glow() {
+        glob.animate({ svgOpacity: 0 }, 2000).animate({ svgOpacity: 1 }, 2000, glow);
+    }
+
+    glow();
+
+
+    d.resolve();
+    return d.promise();
+
 };
 
-Slides.start.Hide = function () {
-    return $("p").fadeOut().promise();
+Slides.jameson_nuss_accident.Hide = function () {
+    return $("figure").fadeOut().promise();
 };
 
