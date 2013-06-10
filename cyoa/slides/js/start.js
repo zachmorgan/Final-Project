33a15482f74d43a3b60a52a54a61002b73f179ec
Kaori Price <kaori.price@gmail.com>
2013-06-09 23:17:33 -0700

    added start.sve, updated style.css to have yellow title, etc.

--- cyoa/slides/js/start.js
+++ cyoa/slides/js/start.js
@@ -1,16 +1,17 @@
 ﻿
 Slides.start = new Slide();
-
+var SVG = null;
 Slides.start.Load = function () {
 
     var d = $.Deferred();
 
     //Make sure to add <div id="svgcontainer"></div> 
     $("#svgcontainer").svg(function (svg) {
-
         var svgsToLoad = [];
 
-        //svgsToLoad.push(LoadSvg(svg, "/slides/resources/price_missle01.svg"));
+        SVG = svg;
+
+        svgsToLoad.push(LoadSvg(svg, "/slides/resources/price_person.svg"));
 
         $.when.apply($, svgsToLoad).done(function () {
             d.resolve();
@@ -25,10 +26,31 @@ Slides.start.Unload = function() {
 };
 
 Slides.start.Show = function () {
-    return $("figure").fadeIn().promise();
+    SVG.configure({ viewBox: "0 0 620 460" });
+
+    var director = $("g#person").detach();
+
+    var door = SVG.rect(20, 0, 90, 120,
+    { fill: '#b3b3b3', stroke: '#b3b3b3', strokeWidth: 5 });
+
+    var room = SVG.rect(220, 150, 400, 240,
+    { fill: '#b3b3b3', stroke: '#b3b3b3', strokeWidth: 5 });
+
+    var room = SVG.rect(220, 150, 400, 240,
+    { fill: '#b3b3b3', stroke: '#b3b3b3', strokeWidth: 5 });
+
+    var room = SVG.rect(220, 150, 400, 240,
+    { fill: '#b3b3b3', stroke: '#b3b3b3', strokeWidth: 5 });
+
+    var room = SVG.rect(220, 150, 400, 240,
+    { fill: '#b3b3b3', stroke: '#b3b3b3', strokeWidth: 5 });
+
+    director.attr("opacity", "1");
+    director.animate({ svgFill: "#fff" }, 2000);
+    director.appendTo($("svg"));
 };
 
 Slides.start.Hide = function () {
-    return $("figure").fadeOut().promise();
+   
 };
 
