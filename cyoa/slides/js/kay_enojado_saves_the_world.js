94d91b0841b4c533e10bf7ad43502dafa577ee8d
Kay W <kaylaenojado@gmail.com>
2013-06-10 13:55:03 -0700

    animation and sound to missing slide

--- cyoa/slides/js/kay_enojado_saves_the_world.js
+++ cyoa/slides/js/kay_enojado_saves_the_world.js
@@ -1,17 +1,14 @@
 Slides.kay_enojado_saves_the_world = new Slide();
 
-
 Slides.kay_enojado_saves_the_world.Load = function () {
     var d = $.Deferred();
-
     //Make sure to add <div id="svgcontainer"></div> 
     $("#svgcontainer").svg(function (svg) {
 
         var svgsToLoad = [];
-        
         svgsToLoad.push(LoadSvg(svg, '/slides/resources/kay_enojado_asteroid_destroyed.svg'));
         svgsToLoad.push(LoadSvg(svg, '/slides/resources/kay_enojado_saves_the_world.svg'));
-       
+
         $.when.apply($, svgsToLoad).done(function () {
             d.resolve();
         });
@@ -21,35 +18,36 @@ Slides.kay_enojado_saves_the_world.Load = function () {
 };
 
 Slides.kay_enojado_saves_the_world.Unload = function() {
-    //if I get the time to put in Audio, it will go here
-    //$('audio').each(function (){
-      //  this.pause();
+        $('audio').each(function (){
+        this.pause();
     });
 };
 
 Slides.kay_enojado_saves_the_world.Show = function () {
+    var asteroiddestroyed = $("g#asteroidgoesboom");
+    var savesworld = $("g#worldissaved");  
+     //var myAudio = document.getElementById('applause');
+    //myAudio.oncanplaythrough = function () { this.play(); }
+        $("audio").each(function () {
+        this.play();
+    });
 
-    var asteroidboom = $("g#asteroidgoesboom");
-    var worldsaved = $("g#worldissaved");
-    //var myAudio = document.getElementById('')
-    //myAudio= function () { this.play()}; 
+    savesworld.attr("transform", "translate(480,300) scale(1.5,1.5)").hide();
+    asteroiddestroyed.attr("transform", "translate(470,300) scale(1.5,1.5)");
+    
+    $('svg').append(savesworld);
 
-    asteroidboom.attr("transform", "translate(480,300) scale(1.5,1.5)");
-    worldsaved.attr("transform", "translate(480,300) scale(1.5,1.5)").hide();
-
-    $('svg').append(worldsaved);
-
-    asteroidboom.attr("transform", "translate(500,320) scale(1.5,1.5)").fadeIn(6000); 
-    //function(){myAudio.play()};
-    worldsaved.attr("transform", "translate(480,300) scale(1.5,1.5)").fadeIn(10000);
+    asteroiddestroyed.attr("transform", "translate(500,300) scale(1.5,1.5)");
+    savesworld.attr("transform", "translate(480,300) scale(1.5,1.5)").fadeIn(2000); 
 
     var d = $.Deferred();
-	d.resolve();
-	return d.promise();
-
+    d.resolve();
+    return d.promise();
 };
 
 Slides.kay_enojado_saves_the_world.Hide = function () {
     return $("figure").fadeOut().promise();
 };
+
+
 
